273b2bc281e1537d0dbb58bf504ec96828352247
mahmoud swedani <mahmoudkasimswedani@gmail.com>
2021-09-22 23:13:42 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import First from './component/first/First';
+function App() {
+  return (
+    <div className="App">
+      <First />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/first/First.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class First extends Component {
+    render() {
+        return (
+            <>
+                <h1>Heloo, world</h1>
+            </>
+        )
+    }
+}
